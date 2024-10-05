fb94ce5bb6dd68497f42c82d45ea89a746398d42
qunthunnan <bredtv6@gmail.com>
2024-10-05 22:32:09 +0300

    methods decorators, decorators 5.0

--- decorators.ts
+++ decorators.ts
@@ -1,20 +1,63 @@
-const myCar = {
-	fuel: "100%",
-	open: true,
-	freeSeats: 3,
-	isOpen() {
+interface IMyCar {
+	fuel: string;
+	open: boolean;
+	freeSeats: number;
+}
+
+@closeCar(false)
+@addFuel(35) //closeCar(addFuel())
+class MyCar implements IMyCar {
+	fuel = "50%";
+	open = true;
+	freeSeats: number;
+
+	@extraIsOpen
+	isOpen ():boolean {
+		console.log(this.fuel);
 		return this.open;
-	},
-};
-
-function closeCar(car: typeof myCar) {
-	car.open = false;
-	return car;
+	}
 }
 
-function addFuel(car: typeof myCar) {
-	car.fuel = "150%";
-	return car;
+function extraIsOpen(
+	target: Object,
+	propertyKey: string | symbol, 
+	descriptor: PropertyDescriptor
+) {
+	const oldValue = descriptor.value;
+	descriptor.value = () => {
+		console.log('Extra actions...');
+		oldValue();
+	}
 }
 
-console.log(addFuel(closeCar(myCar)).isOpen());
+function closeCar(arg: boolean) {
+	return <T extends { new (...arg: any[]): {} }>(constructor: T) => {
+		console.log("closeCar");
+		return class extends constructor {
+			open = arg;
+		};
+	};
+}
+
+// function closeCar(constructor: Function) {
+// 	constructor.prototype.open = false;
+// }
+
+// function closeCar<T extends { new (...arg: any[]): {} }>(constructor: T) {
+// 	return class extends constructor {
+// 		open = false;
+// 	};
+// }
+
+function addFuel(fuel: number) {
+	return <T extends { new (...arg: any[]): {} }>(constructor: T) => {
+		console.log("addFuel");
+		return class extends constructor {
+			fuel = `${fuel}%`;
+		};
+	};
+}
+
+const myCar = new MyCar();
+
+console.log(myCar.isOpen());
